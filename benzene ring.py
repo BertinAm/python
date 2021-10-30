4ecbf929fd53f821b5db8d160c1ce51098ca8995
xaster-Kies <samueledward40@gmail.com>
2021-10-30 21:38:13 +0100

    I changed the bertin variable back to alex

--- benzene ring.py
+++ benzene ring.py
@@ -2,13 +2,15 @@
 #code to draw a benzen ring using python
 import turtle
 window=turtle.Screen()
-bertin=turtle.Turtle()
-bertin.shape("turtle")
+alex=turtle.Turtle()
+alex.shape("turtle")
 for i in range(6):
-    bertin.forward(50)
-    bertin.left(60)
+    alex.forward(50)
+    alex.left(60)
 
 window.mainloop()
+
+
 
     
 
