7ebaaddf0850d3444df4f163a5557eb394a98b60
Unix IO <84545108+BertinAm@users.noreply.github.com>
2022-03-07 20:50:29 +0100

    Add files via upload

---
+++ day_num_rev.py
@@ -0,0 +1,18 @@
+day = input("Enter a day of the week (in small letters) \n")
+
+if day == "sunday":
+    print("Day" + " 0")
+elif day == "monday":
+    print("DAY" + " 1")
+elif day == "tuesday":
+    print("DAY" + " 2")
+elif day == "wednesday":
+    print("DAY" + " 3")
+elif day == "thursday":
+    print("DAY" + " 4")
+elif day == "friday":
+    print("DAY" + " 5")
+elif day == "saturday":
+    print("DAY" + " 6")
+else:
+    print("You did not enter the required format of the day or you entered a non-valid response!")
