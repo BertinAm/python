3187ad839089daac852d30066216ba93e52bdf0e
BertinAm <bertinfonge0gmail.com>
2022-08-03 09:41:17 +0100

    code challenge

--- 50DAYSOFCODECHALLENGE/Day38/Day38.py
+++ 50DAYSOFCODECHALLENGE/Day38/Day38.py
@@ -1,18 +1,20 @@
-def guess_a_number(number):
+def guess_a_number():
     import random as rd
 
     random_number = rd.randrange(1, 100)
     count = 0
-    while count <= 3:
+    while count < 4:
+        number = int(input("Guess my number \n"))
         if number == random_number:
-            return f"{number} is my number. You won!"
+            print(f"{number} is my number. You won!")
+            break
         elif number < random_number:
-            return f"{number} is not my number. Too low!"
+            print(f"{number} is not my number. Too low!")
         elif number > random_number:
-            return f"{number} is not my number. Too high!"
+            print(f"{number} is not my number. Too high!")
 
         count += 1
     print("You Lose!")
 
 
-print(guess_a_number(int(input("Guess my number \n"))))
+guess_a_number()
