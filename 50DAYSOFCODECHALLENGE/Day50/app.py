76f11bf7cc7014b1962745748a93fb3e0a8438e9
BertinAm <bertinfonge0@gmail.com>
2022-09-30 05:26:48 +0100

    code challenge

---
+++ 50DAYSOFCODECHALLENGE/Day50/app.py
@@ -0,0 +1,13 @@
+from email.policy import default
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+if __name__ == "__main__":
+    app.run(debug=True)
