8b5ecdf7f78dd4f9ff1ae078345aa090c3cad2cf
BertinAm <bertinfonge0@gmail.com>
2023-02-08 10:09:39 +0100

    day 1

---
+++ 100DAYSOFCODE Python/Day1/band_name_generator.py
@@ -0,0 +1,8 @@
+# Greeting for my program
+print("Welcome to my Band Name Generator\n")
+# Asking the user for the city he grew up in
+name = input("Enter the name of the city you grew up in:\n")
+# Asking the user for the name of their pet
+pet_name = input("Enter the name of your pet:\n")
+# Printing the name of the band
+print("Your band name could be " + name + " " + pet_name)
