b1cf14518d30a21db3e244c9114698a5e525a613
Unix IO <84545108+BertinAm@users.noreply.github.com>
2023-02-10 23:08:16 +0100

    day 3

--- 100DAYSOFCODE Python/Day3/treasure_island.py
+++ 100DAYSOFCODE Python/Day3/treasure_island.py
@@ -23,7 +23,6 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
-#Write your code below this line 👇
 # Asking the user for the direction
 direction = input('You"re at a crossroad. Where do you want to go? Type "left" or "right" ').lower()
 # if the user goes left enter the if statement
